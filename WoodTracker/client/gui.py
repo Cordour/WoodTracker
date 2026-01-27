f1f48eec77e5c7087a5189b7d42b243f5d8f100a
Cordour <147515849+Cordour@users.noreply.github.com>
2026-01-27 23:20:50 +0100

    v1.2.1 stable

--- WoodTracker/client/gui.py
+++ WoodTracker/client/gui.py
@@ -25,7 +25,7 @@ from CraftCostCalculator import run as run_craft_cost
 GITHUB_VERSION_URL = "https://raw.githubusercontent.com/Cordour/WoodTracker/main/version.json"
 
 
-APP_VERSION = "1.2.0"
+APP_VERSION = "1.2.1"
 
 def check_update():
     r = requests.get(GITHUB_VERSION_URL, timeout=5)
